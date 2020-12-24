add3d38d30083df32de37a4a738a66896526eb0c
martynasd123 <dargis.martynas@gmail.com>
2020-12-24 14:57:06 +0200

    minimum/maximum angle for cannon

--- src/game/component/CannonController.js
+++ src/game/component/CannonController.js
@@ -4,10 +4,13 @@ import {globals} from "../Core";
 const MOVEMENT_SPEED_VERTICAL = 0.005;
 const MOVEMENT_SPEED_HORIZONTAL = 0.01;
 
+const MAX_VERTICAL_ROTATION_DEGREES = -34;
+const MIN_VERTICAL_ROTATION_DEGREES = 0;
+
 /**
  * A component, that is responsible for cannon controls.
  */
-export class CannonController extends Component{
+export class CannonController extends Component {
 
   constructor(entity) {
     super(entity);
@@ -16,16 +19,26 @@ export class CannonController extends Component{
   }
 
   update() {
-    const { inputManager } = globals;
+    const {inputManager} = globals;
 
-    if(inputManager.keys.up.down)
-      this.pipeMesh.rotation.set(0,0,this.pipeMesh.rotation.z - MOVEMENT_SPEED_VERTICAL)
-    else if(inputManager.keys.down.down)
-      this.pipeMesh.rotation.set(0,0,this.pipeMesh.rotation.z + MOVEMENT_SPEED_VERTICAL)
+    const boundRotation = (rotation) => {
+      return Math.max(
+          Math.PI * 2 / 360 * MAX_VERTICAL_ROTATION_DEGREES,
+          Math.min(
+              Math.PI * 2 / 360 * MIN_VERTICAL_ROTATION_DEGREES,
+              rotation
+          )
+      );
+    }
 
-    if(inputManager.keys.right.down)
-      this.entity.visual.rotation.set(0,this.entity.visual.rotation.y - MOVEMENT_SPEED_HORIZONTAL, 0)
-    else if(inputManager.keys.left.down)
-      this.entity.visual.rotation.set(0,this.entity.visual.rotation.y + MOVEMENT_SPEED_HORIZONTAL, 0)
+    if (inputManager.keys.up.down)
+      this.pipeMesh.rotation.set(0, 0, boundRotation(this.pipeMesh.rotation.z - MOVEMENT_SPEED_VERTICAL));
+    else if (inputManager.keys.down.down)
+      this.pipeMesh.rotation.set(0, 0, boundRotation(this.pipeMesh.rotation.z + MOVEMENT_SPEED_VERTICAL));
+
+    if (inputManager.keys.right.down)
+      this.entity.visual.rotation.set(0, this.entity.visual.rotation.y - MOVEMENT_SPEED_HORIZONTAL, 0)
+    else if (inputManager.keys.left.down)
+      this.entity.visual.rotation.set(0, this.entity.visual.rotation.y + MOVEMENT_SPEED_HORIZONTAL, 0)
   }
 }
